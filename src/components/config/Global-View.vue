315aafb0c84421adfc37c41ec6bb717d2a04fb42
hungming0000 <109592528+hungming0000@users.noreply.github.com>
2022-11-21 16:39:31 +0800

    change api address

--- src/components/config/Global-View.vue
+++ src/components/config/Global-View.vue
@@ -1,7 +1,7 @@
 <script>
 // 定義全域變數
 const _token = "";
-const ApiUrl = "https://localhost:44302/";
+const ApiUrl = "https://www.hungminginfo.com/pikegame_API";
 export default {
   _token, //使用者地址
   ApiUrl,
