4896bd2b86484913c20b14a47e6f8d8f0f0f530c
hungming0000 <109592528+hungming0000@users.noreply.github.com>
2022-12-01 16:33:56 +0800

    修改路徑
    修改路徑

--- src/components/config/Global-View.vue
+++ src/components/config/Global-View.vue
@@ -1,8 +1,8 @@
 <script>
 // 定義全域變數
 const _token = "";
-const ApiUrl = "https://localhost:44302/";
-//const ApiUrl = "https://www.hungminginfo.com/pikegame_API";
+//const ApiUrl = "https://localhost:44302/";
+const ApiUrl = "https://www.hungminginfo.com/pikegame_API";
 export default {
   _token, //使用者地址
   ApiUrl,
